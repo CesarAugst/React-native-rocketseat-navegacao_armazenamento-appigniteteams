756026a2aac6f87156ce3979721fd092058b67eb
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-24 10:01:06 -0300

    utilizando theme

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export const Container = styled(View)`
     flex: 1;
-    background-color: #000;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     align-items: center;
     justify-content: center;
 `;
