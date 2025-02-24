f3509632335d51986a24fc5a4d28767f0aaa1b61
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-24 10:03:58 -0300

    otimizando utilização do tema no index usando token de cores

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,11 +4,5 @@ import { View, Text } from 'react-native';
 export const Container = styled(View)`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-    align-items: center;
-    justify-content: center;
-`;
-
-export const Title = styled(Text)`
-    color: #fff;
-    font-size: 32px;
+    padding: 24px;
 `;
