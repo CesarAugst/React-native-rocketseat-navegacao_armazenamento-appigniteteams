f7ac9d51069ffca12a456c61f2c9e160bd73d18a
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-24 18:43:47 -0300

    criando o groupcard mesclando tipagem de componente react-native com personalizado do styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,15 @@
 import {Container} from "./styles";
 import {Header} from "@components/Header";
 import {HighLight} from "@components/HighLight";
+import {GroupCard} from "@components/GroupCard";
 
 export function Groups() {
     return (
         <Container>
             <Header/>
             <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
+            
+            <GroupCard title="Galera do Ignite"/>
         </Container>
     );
 }
