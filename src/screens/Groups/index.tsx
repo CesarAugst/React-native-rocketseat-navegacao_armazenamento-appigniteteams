7c0c7671ee6ddfab785fdef0aac0e54315b8eef9
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-24 19:12:00 -0300

    listando

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,22 @@ import {Container} from "./styles";
 import {Header} from "@components/Header";
 import {HighLight} from "@components/HighLight";
 import {GroupCard} from "@components/GroupCard";
+import {useState} from "react";
+import {FlatList} from "react-native";
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'Família']);
+
     return (
         <Container>
             <Header/>
             <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
             
-            <GroupCard title="Galera do Ignite"/>
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => <GroupCard title={item} />}
+            />
         </Container>
     );
 }
