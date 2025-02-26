c41c2aab0f9e3953f7b39663a95cf780f62f98b8
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-26 18:55:39 -0300

    componente com lista vazia na listagem de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import {HighLight} from "@components/HighLight";
 import {GroupCard} from "@components/GroupCard";
 import {useState} from "react";
 import {FlatList} from "react-native";
+import {ListEmpty} from "@components/ListEmpty";
 
 export function Groups() {
-    const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'Família']);
+    const [groups, setGroups] = useState<string[]>([]);
 
     return (
         <Container>
@@ -17,6 +18,12 @@ export function Groups() {
                 data={groups}
                 keyExtractor={(item) => item}
                 renderItem={({item}) => <GroupCard title={item} />}
+                contentContainerStyle={groups.length === 0 && {flex:1}}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Que tal cadastrar a primeira turma?"
+                    />
+                )}
             />
         </Container>
     );
