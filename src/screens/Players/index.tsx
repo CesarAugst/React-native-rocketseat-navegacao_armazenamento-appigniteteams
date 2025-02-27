ab1b84478b87f6eb5d15b517b31fb6342a0e4950
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-27 17:32:26 -0300

    repassando propriedades dinamicamente para IconButton e criando input de nome de pessoas para os grupos

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
-import {Container} from "@screens/Players/styles";
+import {Container, Form} from "@screens/Players/styles";
 import {Header} from "@components/Header";
 import {HighLight} from "@components/HighLight";
+import {ButtonIcon} from "@components/ButtonIcon";
+import {Input} from "@components/Input";
 
 export function Players(){
     return(
@@ -8,6 +10,11 @@ export function Players(){
             <Header showBackButton />
             
             <HighLight title="Nome da turma" subtitle="adicione a galera e separe os times" />
+
+            <Form>
+                <Input placeholder="Nome da Pessoa" autoCorrect={false} />
+                <ButtonIcon icon="add" />
+            </Form>
         </Container>
     )
 }
