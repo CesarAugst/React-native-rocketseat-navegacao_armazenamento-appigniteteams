971d1471fe23fc70932a52696e63e33790630bdc
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-28 15:29:14 -0300

    utilizando rotas na aplicaçao e removendo o header principal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {Groups} from "@screens/Groups";
+import {Routes} from "@routes/index";
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto";
 
 import { ThemeProvider } from 'styled-components/native'
@@ -16,7 +16,7 @@ export default function App() {
             backgroundColor="transparent"
             translucent
           />
-          { fontsLoaded ? <Groups /> : <Loading/> }
+          { fontsLoaded ? <Routes /> : <Loading/> }
       </ThemeProvider>
   );
 }
