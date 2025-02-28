29778054362e432a8bedb7d317888faec8399064
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2025-02-28 16:35:33 -0300

    utilizando metodos de navageação

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,16 @@ import {Header} from "@components/Header";
 import {HighLight} from "@components/HighLight";
 import {Button} from "@components/Button";
 import {Input} from "@components/Input";
+import {useNavigation} from "@react-navigation/native";
+import {useState} from "react";
 
 export function NewGroup(){
+    const [group, setGroup] = useState("");
+    const navigation = useNavigation();
+
+    function handleNew(){
+        navigation.navigate("players", { group});
+    }
     return(
         <Container>
             <Header showBackButton />
@@ -12,8 +20,8 @@ export function NewGroup(){
             <Content>
                 <Icon />
                 <HighLight title="Nova turma" subtitle="crie a turma para adicionar as pessoas" />
-                <Input placeholder="Nome da Turma"/>
-                <Button title="criar" style={{marginTop: 20}}/>
+                <Input placeholder="Nome da Turma" onChangeText={setGroup}/>
+                <Button title="criar" style={{marginTop: 20}} onPress={handleNew}/>
             </Content>
         </Container>
     )
